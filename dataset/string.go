035eef3308edd4ec581061440337aaa10a37f573
Jonas Knobloch <jonas.knobloch@t-online.de>
2026-03-17 19:13:32 +0100

    Add string reader

---
+++ dataset/string.go
@@ -0,0 +1,25 @@
+package dataset
+
+import "iter"
+
+type StringReader struct {
+	s string
+}
+
+func NewStringReader(s string) *StringReader {
+	return &StringReader{
+		s: s,
+	}
+}
+
+func (s *StringReader) Texts(_ string) iter.Seq[string] {
+	return func(yield func(string) bool) {
+		if !yield(s.s) {
+			return
+		}
+	}
+}
+
+func (s *StringReader) Err() error {
+	return nil
+}
